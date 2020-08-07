dfb7563d5ed8185395752e9a38ed7dd5ca052736
JCJ <jesse.jackson.c@gmail.com>
2020-08-07 05:16:28 -0500

    created the foundation to place 'random' bombs throughout the grid

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
   let width = 10;
   let squares = [];
+  let bombAmount = 20;
 
   //Function to create the board
   function createBoard() {
+
+    //Randomizing bombs to stick within the board
+    const bombsArray = Array(bombAmount).fill("bomb");                          //Creating array of 'bomb' strings to place in divs
+    const emptyArray = Array(width*width - bombAmount).fill("valid");           //Creating array of 'valid' strings to place in divs
+    const gameArray = emptyArray.concat(bombsArray);                            //Joining the two 'bomb' and 'valid' arrays
+    const shuffledArray = gameArray.sort(() => Math.random() -0.5);             //Randomly shuffling 'bomb' and 'valid' within the array
+
     for (let i = 0; i < width * width; i++) {
       const square = document.createElement("div");            //Creating div squares for our board
-      square.setAttribute("id", i);                            //Labeling each div with an ID as the loop runs
+      square.setAttribute("id", i);                            //Labeling each div with a unique ID as the loop runs
       grid.appendChild(square);                                //Appending new divs into our grid
       squares.push(square);                                    //Assigning new divs into our global array
     }
